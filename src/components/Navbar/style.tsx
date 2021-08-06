48f6bf75c4578eb2d2f26bb0542949075e71cf5f
Renan Paixão <renanlemos21@gmail.com>
2021-08-06 20:25:30 -0300

    feat(navbar): adicionada responsividade

--- src/components/Navbar/style.tsx
+++ src/components/Navbar/style.tsx
@@ -8,6 +8,22 @@ export const Container = styled.nav`
   padding: .3rem 1rem;
 
   background-color: var(--white);
+
+  @media(min-width: 576px){
+  padding: .3rem 1rem .3rem 1.7rem;
+
+  }
+  @media(min-width: 720px){
+  padding: .3rem 1rem .3rem 2rem;
+
+  }
+  @media(min-width: 960px){
+  padding: .3rem 1rem .3rem 2.5rem;
+  
+  }
+  @media(min-width: 1200px){
+    padding: .3rem 1rem .3rem 5rem;
+  }
 `;
 
 export const Logo = styled.img`
@@ -15,4 +31,21 @@ export const Logo = styled.img`
   height: 55px;
 
   cursor: pointer;
+
+  @media(min-width: 576px){
+    width: 70px;
+    height: 65px;
+  }
+  @media(min-width: 720px){
+    width: 75px;
+    height: 70px;
+  }
+  @media(min-width: 960px){
+    width: 80px;
+    height: 75px;
+  }
+  @media(min-width: 1200px){
+    width: 90px;
+    height: 80px;
+  }
 `;
